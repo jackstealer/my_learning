67349bf45e3923e1a9545eb9fa8d1955f3b455fe
jackstealer <jackstealer690@email.com>
2026-05-05 23:54:17 +0530

    Add CNN weather classification and CIFAR-10/Fashion MNIST training scripts

--- New folder (3)/image/face_mesh.py
+++ New folder (3)/image/face_mesh.py
@@ -3,11 +3,10 @@ import mediapipe as mp
 
 mp_face_mesh=mp.solutions.face_mesh
 mp_drawing=mp.solutions.drawing_utils
-face_mesh=mp_face_mesh.FaceMesh(static_image_mode=False,
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5)
+face_mesh=mp_face_mesh.FaceMesh(
+    static_image_mode=False,
+    refine_landmarks=True,)
+
 cap=cv2.VideoCapture(0)
 while True:
     flag,frame=cap.read()
@@ -17,13 +16,20 @@ while True:
     results=face_mesh.process(rgb_frame)
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                image=frame,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec=mp_drawing.DrawingSpec(thickness=1,circle_radius=1,color=(0,255,0)),
-                connection_drawing_spec=mp_drawing.DrawingSpec(thickness=1,circle_radius=1,color=(0,255,0))
-            )
+
+            h,w,_=frame.shape
+            point=face_landmarks.landmark[33]
+            x=int(point.x*w)
+            y=int(point.y*h) 
+            cv2.circle(frame,(x,y),5,(255,0,0),2) 
+
+            # mp_drawing.draw_landmarks(
+            #     image=frame,
+            #     landmark_list=face_landmarks,
+            #     connections=mp_face_mesh.FACEMESH_TESSELATION,
+            #     landmark_drawing_spec=mp_drawing.DrawingSpec(thickness=1,circle_radius=1,color=(255,0,255)),
+            #     connection_drawing_spec=mp_drawing.DrawingSpec(thickness=1,circle_radius=1,color=(255,0,255))
+            # )
     cv2.imshow("face mesh",frame)
     key=cv2.waitKey(1) & 0xFF
     if key==27:
